1103059102c2044ef6403e6ab27fce8eeb921987
Arink Verma <arinkverma@gmail.com>
2015-10-28 12:49:15 +0530

    Fix and clean indentation

--- android-native-sample/src/com/greedygame/example/andorid/AnimatedActivity.java
+++ android-native-sample/src/com/greedygame/example/andorid/AnimatedActivity.java
@@ -12,56 +12,56 @@ import com.greedygame.android.FloatAdLayout;
 import com.greedygame.android.GreedyUtilities;
 
 public class AnimatedActivity extends Activity {
-	
+    
 
-	
-	private String themePath = null;
-	private FloatAdLayout floatAdlayout = null;
+    
+    private String themePath = null;
+    private FloatAdLayout floatAdlayout = null;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-    	setContentView(R.layout.activity_animated);
-    	
-    	themePath = SplashScreenActivity.ggAgent.getActivePath();
-    	
-    	/*** Fetching Float Ad unit ***/
-    	floatAdlayout = new FloatAdLayout(this);
-    	Log.i("GreedyGame Sample", "activePath "+themePath);
-		
-    	try {
-			floatAdlayout.fetchHeadAd("unit-363");
-		} catch (AgentInitNotCalledException e) {
-			e.printStackTrace();
-		}
-    	
-    	
-    	/*** Changing Native Ad units ***/
-    	ImageView sun =  (ImageView) findViewById(R.id.sun);
-    	
-    	Bitmap bmp = GreedyUtilities.getBitmapByResid(SplashScreenActivity.ggAgent, R.id.sun);
-    	if(bmp != null){
-    		sun.setImageBitmap(bmp);
-    	}
-	}
-	
-	@Override
-	public void onResume(){
-		super.onResume();
-		SplashScreenActivity.ggAgent.onResume();
-	}
-	
-	@Override
-	public void onDestroy(){
-		super.onDestroy();
-		floatAdlayout.removeHeadAd("unit-363");
-	}
-	
-	
-	@Override
-	public void onPause(){
-		super.onPause();
-		SplashScreenActivity.ggAgent.onPause();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animated);
+        
+        themePath = SplashScreenActivity.ggAgent.getActivePath();
+        
+        /*** Fetching Float Ad unit ***/
+        floatAdlayout = new FloatAdLayout(this);
+        Log.i("GreedyGame Sample", "activePath "+themePath);
+        
+        try {
+            floatAdlayout.fetchHeadAd("unit-363");
+        } catch (AgentInitNotCalledException e) {
+            e.printStackTrace();
+        }
+        
+        
+        /*** Changing Native Ad units ***/
+        ImageView sun =  (ImageView) findViewById(R.id.sun);
+        
+        Bitmap bmp = GreedyUtilities.getBitmapByResid(SplashScreenActivity.ggAgent, R.id.sun);
+        if(bmp != null){
+            sun.setImageBitmap(bmp);
+        }
+    }
+    
+    @Override
+    public void onResume(){
+        super.onResume();
+        SplashScreenActivity.ggAgent.onResume();
+    }
+    
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        floatAdlayout.removeHeadAd("unit-363");
+    }
+    
+    
+    @Override
+    public void onPause(){
+        super.onPause();
+        SplashScreenActivity.ggAgent.onPause();
+    }
 
 }
