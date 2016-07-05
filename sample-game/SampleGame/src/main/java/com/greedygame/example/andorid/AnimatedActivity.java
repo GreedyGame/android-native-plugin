d0df323c49900da68f1d3b91a039f5d82d7083af
niks_nixac <nikhil@greedygame.com>
2016-07-05 14:28:42 +0530

    utilities removed

--- sample-game/SampleGame/src/main/java/com/greedygame/example/andorid/AnimatedActivity.java
+++ sample-game/SampleGame/src/main/java/com/greedygame/example/andorid/AnimatedActivity.java
@@ -3,12 +3,13 @@ package com.greedygame.example.andorid;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager.LayoutParams;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import com.greedygame.android.GreedyUtilities;
+
 import com.greedygame.android.adhead.FloatUnitLayout;
 import com.greedygame.android.exceptions.AgentInitNotCalledException;
 
@@ -41,12 +42,24 @@ public class AnimatedActivity extends Activity {
     	/*** Changing Native Ad units ***/
     	ImageView sun =  (ImageView) findViewById(R.id.sun);
     	
-    	Bitmap bmp = GreedyUtilities.getBitmapByResid(SplashScreenActivity.ggAgent, R.id.sun);
+    	Bitmap bmp = getBitmapFromFileName("sun.png");
     	if(bmp != null){
     		sun.setImageBitmap(bmp);
     	}
 	}
-	
+
+	private Bitmap getBitmapFromFileName(String s) {
+		String file = SplashScreenActivity.ggAgent.getNativeUnitPathByName(s);
+		Bitmap bitmap ;
+		if(file!=null) {
+			bitmap = BitmapFactory.decodeFile(file);
+		} else {
+			bitmap = null;
+		}
+		return bitmap;
+
+	}
+
 	@Override
 	public void onResume(){
 		super.onResume();
