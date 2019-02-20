597b14e415f893139764b9b21a4fede0c520257a
Mathan <mathan.raj43@gmail.com>
2019-02-20 10:02:48 +0530

    Changed the sample app with version 9.0

---
+++ app/src/main/java/com/greedygame/androidsample/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.greedygame.androidsample
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.greedygame.android.agent.GreedyGameAgent
+import com.greedygame.android.core.campaign.CampaignStateListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), CampaignStateListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val greedyGameAgent = GreedyGameAgent.Builder(this)
+                .gameId("00000010")
+                .addUnitId("slot-1000")
+                .addUnitId("slot-1001")
+                .enableAdmob(true)
+                .withAgentListener(this)
+                .build()
+
+        showAd.setOnClickListener {
+            greedyGameAgent?.init()
+        }
+    }
+
+    override fun onAvailable(campaignId: String) {
+        Snackbar.make(findViewById(android.R.id.content), "Ad Available", Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onError(message: String) {
+        Snackbar.make(findViewById(android.R.id.content), "Ad error $message", Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onUnavailable() {
+        Snackbar.make(findViewById(android.R.id.content), "Ad Unavailable", Snackbar.LENGTH_SHORT).show()
+    }
+}
