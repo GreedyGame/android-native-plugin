a63715baa99ba1edb620285960c730e9aad2c6d9
jude <jude.osbert@greedygame.com>
2020-01-30 14:49:03 +0530

    updates examples for 8.8.17-beta

--- app/src/main/java/com/greedygame/androidsample/MainActivity.kt
+++ app/src/main/java/com/greedygame/androidsample/MainActivity.kt
@@ -1,31 +1,49 @@
 package com.greedygame.androidsample
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import com.greedygame.android.agent.AdListener
-import com.greedygame.android.agent.GreedyGameAds
+import android.widget.ImageView
+import com.greedygame.android.agent.GreedyGameAgent
+
+import com.greedygame.android.core.campaign.CampaignStateListener
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), AdListener {
+class MainActivity : AppCompatActivity(), CampaignStateListener {
+    var greedyGameAgent:GreedyGameAgent?=null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val greedyGameAgent = GreedyGameAds.Builder(this)
-                .appId("00000010")
-                .addUnitId("slot-1000")
-                .addUnitId("slot-1001")
+
+        //GreedyGameAgent can be initiated with an Activity or a Context.
+        greedyGameAgent = GreedyGameAgent.Builder(this as Context)
+                .setGameId("66081223")
+                .addUnitId("float-4343")
                 .enableAdmob(true)
-                .withAdListener(this)
+                .withAgentListener(this)
                 .build()
 
         showAd.setOnClickListener {
-            greedyGameAgent?.load()
+            greedyGameAgent?.init()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        greedyGameAgent?.let {
+            if (it.isCampaignAvailable){
+                nativead.loadAd("float-4343")
+            }
+        }
+
     }
 
     override fun onAvailable(campaignId: String) {
         Snackbar.make(findViewById(android.R.id.content), "Ad Available", Snackbar.LENGTH_SHORT).show()
+        //loadAd is an extension function on ImageView
+        nativead.loadAd("float-4343")
     }
 
     override fun onError(message: String) {
@@ -34,5 +52,16 @@ class MainActivity : AppCompatActivity(), AdListener {
 
     override fun onUnavailable() {
         Snackbar.make(findViewById(android.R.id.content), "Ad Unavailable", Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun ImageView.loadAd(unitId:String){
+        val adPath = greedyGameAgent?.getPath(unitId)?:""
+        if(adPath.isNotEmpty()){
+            val bitmap = BitmapFactory.decodeFile(adPath)
+            this.setImageBitmap(bitmap)
+            this.setOnClickListener {
+                greedyGameAgent?.showUII(unitId)
+            }
+        }
     }
 }
